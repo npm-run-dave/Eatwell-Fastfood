3804fc883eaf5017c893997a58f2cc00622695d3
npm-run-dave <ryandavedonos@gmail.com>
2025-08-20 11:46:45 +0800

    Chore: add sample logo

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -1,17 +1,23 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className="bg-blue-600 text-white shadow-md sticky top-0 z-50">
-      <nav className="container mx-auto flex justify-between items-center py-4 px-6 md:px-0">
-        <h1 className="text-2xl font-bold">MyApp</h1>
-
-        {/* Desktop menu */}
+    <header className="bg-[#041420] text-white shadow-md sticky top-0 z-50">
+      <nav className="container mx-auto flex justify-between items-center py-2 px-6 md:px-0">
+        <Image
+          src="/images/Logo.png"
+          alt="MyApp Logo"
+          width={150}
+          height={50}
+          className=" w-[120px] "
+        />
+         
         <ul className="hidden md:flex space-x-6 text-lg">
           <li><a href="/" className="hover:underline">Home</a></li>
           <li><a href="/about" className="hover:underline">About</a></li>
           <li><a href="/contact" className="hover:underline">Contact</a></li>
         </ul>
 
-        {/* Mobile menu button */}
         <div className="md:hidden">
           <button className="focus:outline-none">
             <svg
